2a9f6db60baa5791bd15a58f7834e3cdfaad78be
Joel <joelvizcarra98@gmail.com>
2019-05-13 00:48:18 -0500

    app

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
   //__dirname : It will resolve to your project folder.
 });
+app.get("/about", function(req, res) {
+  res.sendFile(path.join(__dirname + "/about.html"));
+  //__dirname : It will resolve to your project folder.
+});
+app.get("/src/hashes.js", function(req, res) {
+  res.sendFile(path.join(__dirname + "/src/hashes.js"));
+  //__dirname : It will resolve to your project folder.
+});
 app.listen(process.env.PORT || 3000);
 console.log("Server running");
